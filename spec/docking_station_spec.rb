c4b3323e3b2c2800fd7afe84b09835d649d7aa54
Phil <phil.board90@googlemail.com>
2019-03-18 16:20:56 +0000

    added dock_bike method and arrt_reader

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,8 @@ require 'boris_bike'
 
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike) }
-
-  it {subject.release_bike is_expected.to respond_to(:working?) }
-
+  it 'release bike' do
+    expect(subject.release_bike).to respond_to(:working?)
+  end
+  it {is_expected.to respond_to(:dock_bike)}
 end
