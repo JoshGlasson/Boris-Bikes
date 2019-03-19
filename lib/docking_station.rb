6b1176a1f5edff28b03528f53790aa9165079ff1
Josh Glasson <Josh.Glasson@tesco.com>
2019-03-19 09:21:12 +0000

    added tests to fail if dock full or empty

---
+++ lib/docking_station.rb
@@ -0,0 +1,20 @@
+require_relative 'bike'
+
+class DockingStation
+attr_reader :bike
+
+  def initialize
+    @bikes = []
+  end
+
+  def dock(bike)
+    fail 'Docking station full' if @bikes.count >= 20
+    @bikes << bike
+  end
+
+  def release_bike
+    fail 'No bikes available' if @bikes.empty?
+    @bikes.pop
+  end
+
+end
