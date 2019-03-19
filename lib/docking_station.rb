4ce9a7f680b554eef1cbc5dc05db2380abbc1737
Josh Glasson <Josh.Glasson@tesco.com>
2019-03-19 09:56:20 +0000

    added ability to change capacity, but default when left blank

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,31 +1,33 @@
 require_relative 'bike'
 
 class DockingStation
-attr_reader :bikes
 DEFAULT_CAPACITY = 20
+attr_accessor :capacity
+attr_reader :bikes
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @bikes = []
+    @capacity = capacity
   end
 
   def dock(bike)
     fail 'Docking station full' if full?
-    @bikes << bike
+    bikes << bike
   end
 
   def release_bike
     fail 'No bikes available' if empty?
-    @bikes.pop
+    bikes.pop
   end
 
 private
 
   def full?
-    @bikes.count >= DEFAULT_CAPACITY
+    bikes.count >= capacity
   end
 
   def empty?
-    @bikes.empty?
+    bikes.empty?
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -23,6 +23,10 @@ describe DockingStation do
     expect(subject.bikes).to eq [bike]
   end
 
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
   describe '#release_bike' do
     it 'releases a bike' do
       bike = Bike.new
@@ -37,10 +41,25 @@ describe DockingStation do
 
   describe '#dock' do
     it 'raises an error when full' do
-      DockingStation::DEFAULT_CAPACITY.times do
-        subject.dock Bike.new
-      end
+      subject.capacity.times { subject.dock Bike.new }
       expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+
+  describe 'initialization' do
+    it 'has a variable capacity' do
+      docking_station = DockingStation.new(50)
+      50.times { docking_station.dock Bike.new }
+      expect{ docking_station.dock Bike.new }.to raise_error 'Docking station full'
+    end
+
+    subject { DockingStation.new }
+    let(:bike) { Bike.new }
+    it 'defaults capacity' do
+      described_class::DEFAULT_CAPACITY.times do
+        subject.dock(bike)
+      end
+    expect{ subject.dock(bike) }.to raise_error 'Docking station full'
     end
   end
 
